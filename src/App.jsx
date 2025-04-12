9e07d1db18c0ce3cc9826373065ee8368bd7bcbd
Sasha_Ischuk <ipz231_ios1@student.ztu.edu.ua>
2025-04-12 15:31:55 +0300

    dz 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,16 @@
-import { useSelector } from "react-redux";
 import ContactForm from "./components/ContactForm/ContactForm";
 import ContactList from "./components/ContactList/ContactList";
 import SearchBox from "./components/SearchBox/SearchBox";
 import styles from "./App.module.css";
 
 function App() {
-  const contacts = useSelector((state) => state.contacts.items);
-  const filter = useSelector((state) => state.filters.name);
-
-  const getFilteredContacts = () => {
-    const normalizedFilter = filter.toLowerCase();
-    return contacts.filter((contact) =>
-      contact.name.toLowerCase().includes(normalizedFilter)
-    );
-  };
-
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Phonebook</h1>
       <ContactForm />
       <h2 className={styles.subtitle}>Contacts</h2>
       <SearchBox />
-      <ContactList contacts={getFilteredContacts()} />
+      <ContactList />
     </div>
   );
 }
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,10 +1,21 @@
+import { useSelector } from "react-redux";
 import Contact from "../Contact/Contact";
 import styles from "./ContactList.module.css";
 
-const ContactList = ({ contacts }) => {
+const ContactList = () => {
+  const contacts = useSelector((state) => state.contacts.items);
+  const filter = useSelector((state) => state.filters.name);
+
+  const getFilteredContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
   return (
     <ul className={styles.list}>
-      {contacts.map((contact) => (
+      {getFilteredContacts().map((contact) => (
         <li key={contact.id} className={styles.item}>
           <Contact contact={contact} />
         </li>
